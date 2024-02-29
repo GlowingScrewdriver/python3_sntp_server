24add76a961c4deae9fb322f5e4fa268c4c00af9
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2024-02-29 20:00:16 +0530

    Add sntp_server.py
    sntp_server.py now contains a class, `SNTPSocket`, to handle
    socket operations like sending and receiving messages.
    
    demo.py has also been updated with an example to demonstrate the
    usage of `sntp_server.SNTPSocket`. This example constitutes a bare
    minimum of functionality required for an SNTP client to accept the
    server's response.
    This was tested with NTPSec's `ntpdig` utility: https://docs.ntpsec.org/latest/ntpdig.html

--- demo.py
+++ demo.py
@@ -1,8 +1,25 @@
 #!/bin/python3
 import sntp
+import sntp_server
+
+
+### Demo on how to use class sntp_server.SNTPSocket ###
+if __name__ == "__main__":
+    sock = sntp_server.SNTPSocket ()
+    while True:
+        msg = sock.recvSNTP ()
+        print (msg)
+        msg ["Mode"] = 4
+        msg ["Stratum"] = 15
+        msg ["LI"] = 0
+        msg ["RecieveTimestamp"] = msg ["TransmitTimestamp"]
+        msg ["OriginateTimestamp"] = msg ["TransmitTimestamp"]
+        sock.sendSNTP (msg)
+
+exit ()
+
 
 ### Demo on how to use class sntp.SNTPMsg ###
-
 if __name__ == "__main__":
     # Constructing a message object
     msg = sntp.SNTPMsg ()
---
+++ sntp_server.py
@@ -0,0 +1,30 @@
+"""
+This module defines classes to handle the communication operations of
+the SNTP server, like sending and receiving messages.
+"""
+
+from sntp import SNTPMsg
+from socket import (
+    socket,
+    AF_INET, SOCK_DGRAM,
+)
+
+class SNTPSocket (socket):
+    def __init__ (self):
+        socket.__init__ (self, AF_INET, SOCK_DGRAM) # UDP/IP socket
+        self.bind (("127.0.0.1", 123))              # Standard port number for SNTP
+
+
+    def recvSNTP (self):
+        """
+        Return a single SNTP request from the socket
+        """
+        return SNTPMsg (* self.recvfrom (1024))
+
+
+    def sendSNTP (self, msg):
+        """
+        Send an SNTP response back to the source of the corresponding request
+        msg: an SNTPMsg object
+        """
+        self.sendto (msg, msg.addr)
