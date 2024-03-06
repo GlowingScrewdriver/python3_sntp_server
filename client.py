a9b7d1a213a3cb59540e98aafd85dd5ca89d8213
Vignesh Singaravelu <143326541+usefulldivider@users.noreply.github.com>
2024-03-06 20:17:29 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket, sntp, time
-from _datetime import datetime,timedelta
+from _datetime import datetime, timedelta
 import win32api
+import conversions
+
 if __name__ == "__main__":
     ntp_servers = {
         "time.google.com": ("time.google.com", 123),
@@ -13,6 +15,17 @@ if __name__ == "__main__":
     selected_ntp_server = "time.google.com"
     server_addr = ntp_servers[selected_ntp_server]
 
+# the below function sets the clock of the local system
+    def setsystime(time_str):
+        '''takes time in string and sets it to the locale system ->use time.ctime()'''
+        ntp_dt = datetime.strptime(time_str, "%a %b %d %H:%M:%S %Y")
+        ist_dt = ntp_dt - timedelta(hours=5, minutes=30)
+        time_t = (ist_dt.year, ist_dt.month, ist_dt.day,
+                  ist_dt.hour, ist_dt.minute, ist_dt.second, 0)
+        dayofweek = datetime(*time_t).isocalendar()[2]
+        t = time_t[:2] + (dayofweek,) + time_t[2:]
+        win32api.SetSystemTime(*t)
+
 
     # Create a UDP socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as c_sock:
@@ -21,15 +34,14 @@ if __name__ == "__main__":
         hserver_req["VN"] = 4
         hserver_req["Mode"] = 3  # 3-> client coz its sending request to (hserver)higher server
         hserver_req["Stratum"] = 15  # 2-15 -> secondary reference server
-        hserver_req["OriginateTimestamp"] = 3918579230  # 04/03/2024, 10:22:40 PM
-        hserver_req["TransmitTimestamp"] = 3918579230  # hard coded coz cant convert to ntp time format for now
+        hserver_req["OriginateTimestamp"] = conversions.posix_to_ntp(time.time()) # time when the message departs
+        hserver_req["TransmitTimestamp"] = conversions.posix_to_ntp(time.time()) # time when the message departs
 
         c_sock.sendto(hserver_req, server_addr)
         hserver_res, addr = c_sock.recvfrom(48)
         hserver_res = sntp.SNTPMsg(hserver_res)
         if hserver_res["OriginateTimestamp"] == hserver_req["TransmitTimestamp"]:
             # the above check is done for reply attacks
-            '''hserver_res["LI"] != 0 or '''
             if hserver_res["Stratum"] == 0 or hserver_res["TransmitTimestamp"] == 0 or (
                     hserver_res["Mode"] != 4 and hserver_res["Mode"] != 5):
                 print("Server reply is discarded\nDoes not comply with message standard specified by rfc")
@@ -37,24 +49,29 @@ if __name__ == "__main__":
                 print("Processing server reply")
                 print(f"The current primary server:{server_addr[0]}\nThe server response message :\n{hserver_res}")
                 # calculating round trip delay
-                # t1 = hserver_req["OriginateTimestamp"]
-                # t2 = hserver_res["RecieveTimestamp"]
-                # t3 = hserver_res["TransmitTimestamp"]
-                # t4=
-                # d=(t4-t1)-(t3-t2)
+                t1 = hserver_req["OriginateTimestamp"]
+                t2 = hserver_res["RecieveTimestamp"]
+                t3 = hserver_res["TransmitTimestamp"]
+                t4 = conversions.posix_to_ntp(time.time())
+                # print(time.ctime(conversions.ntp_to_posix(t1)))
+                # print(time.ctime(conversions.ntp_to_posix(t2)))
+                # print(time.ctime(conversions.ntp_to_posix(t3)))
+                # print(time.ctime(conversions.ntp_to_posix(t4)))
 
-                # accurate_time = hserver_res["TransmitTimestamp"] - (d / 2)
-                # setsystime()
-                # print(hserver_res["TransmitTimestamp"]>>32)
+                # print(f"t4:{t4},{time.ctime(conversions.ntp_to_posix(t4))}")
+                # d is the rtd 
+                d = (t4 - t1) - (t3 - t2)
+                # t is the clock offset
+                t = ((t2 - t1) + (t3 - t4)) / 2
 
+                # the below transmit time is the request one same as local time
+                accurate_time = hserver_req["TransmitTimestamp"] + int(t) -int((d / 2))
+                ist_accurate_time = conversions.ntp_to_posix(accurate_time) + (5 * 3600) + (30 * 60)
+                print(ist_accurate_time,accurate_time)
 
+                print(f"accurate time : {time.ctime(conversions.ntp_to_posix(accurate_time))},current time {time.ctime()}")
+                print(f"the supposed ist time {time.ctime(ist_accurate_time)}")
+                print(f"Transmit time ins response {time.ctime(conversions.ntp_to_posix(t3))}")
 
-def setsystime(time_str="Sun Mar 3 23:25:32 2024"):
-    '''takes time in string and sets it to the locale system ->use time.ctime()'''
-    ntp_dt = datetime.strptime(time_str, "%a %b %d %H:%M:%S %Y")
-    ist_dt = ntp_dt - timedelta(hours=5, minutes=30)
-    time_t = (ist_dt.year, ist_dt.month, ist_dt.day,
-              ist_dt.hour, ist_dt.minute, ist_dt.second, 0)
-    dayofweek = datetime(*time_t).isocalendar()[2]
-    t = time_t[:2] + (dayofweek,) + time_t[2:]
-    win32api.SetSystemTime(*t)
+                # setsystime(time.ctime(conversions.ntp_to_posix(accurate_time)))
+
